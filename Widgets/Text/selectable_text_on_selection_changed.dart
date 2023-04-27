1dc9aa507259655f7c5658466c56c039c332142e
Nasibali <72644180+Nasibali144@users.noreply.github.com>
2023-04-27 08:26:35 +0500

    Create selectable_text_on_selection_changed.dart

---
+++ Widgets/Text/selectable_text_on_selection_changed.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SelectableText(
+                "PDP IT Academy: Flutter Development Group",
+                style: const TextStyle(
+                  fontSize: 27.5,
+                ),
+                textWidthBasis: TextWidthBasis.longestLine,
+                showCursor: true,
+                autofocus: true,
+                onSelectionChanged: (textSelection, selectionChangedCause) {
+                  debugPrint(textSelection.toString());
+                  debugPrint(selectionChangedCause.toString());
+                },
+              ),
+              const SizedBox(height: 15,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
